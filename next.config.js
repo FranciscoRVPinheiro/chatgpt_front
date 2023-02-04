04c2aab32e7c5642b90c13d6ed0a07ac9b9d97e3
Ricardo <franciscorvpinheiro49@gmail.com>
2023-02-04 20:01:32 -0300

    pain3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+// });
 
-module.exports =  withPWA({
+// module.exports =  withPWA({
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "lh3.googleusercontent.com",
+//         port: "",
+//         pathname: "/a/**",
+//       },
+//     ],
+//   },
+// });
+
+module.exports = {
   images: {
     remotePatterns: [
       {
@@ -13,4 +26,4 @@ module.exports =  withPWA({
       },
     ],
   },
-});
+};
