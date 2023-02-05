13c84a63e0466557e7dc852f4677768d7c349d5f
Ricardo <franciscorvpinheiro49@gmail.com>
2023-02-04 22:35:22 -0300

    useSession deploy test

--- components/ChatLine.tsx
+++ components/ChatLine.tsx
@@ -1,5 +1,8 @@
 // @ts-nocheck
 import Balancer from "react-wrap-balancer";
+import { useSession } from "next-auth/react";
+
+
 
 // wrap Balancer to remove type errors :( - @TODO - fix this ugly hack
 const BalancerWrapper = (props: any) => <Balancer {...props} />;
@@ -26,6 +29,16 @@ const convertNewLines = (text: string) =>
 
 export function ChatLine({ who = "bot", message }: Message) {
 
+  const { data: newSession } = useSession();
+
+  let mySelf;
+
+  if (newSession) {
+    mySelf = newSession.user.name;
+  } else {
+    mySelf = "Me";
+  }
+
   if (!message) {
     return null;
   }
@@ -43,7 +56,7 @@ export function ChatLine({ who = "bot", message }: Message) {
             <div className="flex space-x-5">
               <div className="flex-1 gap-4">
                 <p className="text-sm text-slate-300 font-mono">
-                  {who == "bot" ? "Jarvis" : "me" }
+                  {who == "bot" ? "Jarvis" : mySelf }
                 </p>
                 <p className="text-white text-lg font-mono">
                   {formatteMessage}
