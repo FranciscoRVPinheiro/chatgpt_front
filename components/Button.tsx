6af46207b4bb4665214cb1307e83572633b762e5
Ricardo <franciscorvpinheiro49@gmail.com>
2023-02-05 08:40:11 -0300

    fixed button margin

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ export function Button({ className, ...props }: any) {
   return (
     <button
       className={clsx(
-        "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 mr-2 text-sm outline-offset-2 transition active:transition-none",
+        "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none",
         "bg-indigo-600 font-semibold text-zinc-100 hover:bg-indigo-500 active:bg-zinc-800 active:text-zinc-100/70",
         className
       )}
