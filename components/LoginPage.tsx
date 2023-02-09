a98b3b3e0809170025913f56e1d1ea2a98ef963f
Ricardo <franciscorvpinheiro49@gmail.com>
2023-02-09 16:08:03 -0300

    added loading to avoid flickering

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -1,9 +1,14 @@
+import { useSession } from "next-auth/react";
+
 export default function LoginPage() {
-    return (
-      <div className="flex justify-center items-center h-screen bg-neutral-900">
-        <p className="text-white font-mono text-xl animate-pulse">
-          Login to use Jarvis
-        </p>
-      </div>
-    );
+   
+  const { status } = useSession();  
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-neutral-900">
+      <p className="text-white font-mono text-xl animate-pulse">
+        {status === "loading" ? "" : "Login to use Jarvis"}
+      </p>
+    </div>
+  );
 }
